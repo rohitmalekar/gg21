d074011df3cdc5c51819d99331f885c7f2f02d1e
rohitmalekar <rohitmalekar@gmail.com>
2024-08-05 11:26:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,4 +19,4 @@ minutes, seconds = divmod(remainder, 60)
 
 st.info(f"Time until launch: {days} days, {hours} hours, {minutes} minutes, {seconds} seconds")
 
-st.markdown("Check out (grantsscope.xyz)[grantsscope.xyz] or the project's (Karma GAP page)[https://gap.karmahq.xyz/project/grantsscope---grantee-discovery-using-llms] for the product journey over the last year!")
+st.markdown("Check out [grantsscope.xyz](grantsscope.xyz) or the project's [Karma GAP page](https://gap.karmahq.xyz/project/grantsscope---grantee-discovery-using-llms) for the product journey over the last year!")
